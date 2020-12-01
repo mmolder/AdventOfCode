24bce7e51104ef643b5e20bc2300fdcfd68e6cc5
Mikael Mölder <mikael@molder.se>
2020-12-01 10:08:04 +0100

    FIX: code put in function

--- 2020/1_2.py
+++ 2020/1_2.py
@@ -5,16 +5,19 @@ with open("2020/input_1_1.csv", 'r') as file:
 
 content = sorted(content, key=lambda x: x)
 
-for i in range(len(content)):
-    j = i + 1
-    z = len(content) - 1
-    while j < z:
-        tmp = content[j] + content[z] + content[i]
-        if tmp > 2020:
-            z -= 1
-        elif tmp < 2020:
-            j += 1
-        else:
-            print(content[j] * content[z] * content[i])
-            break
 
+def task2():
+    for i in range(len(content)):
+        j = i + 1
+        z = len(content) - 1
+        while j < z:
+            tmp = content[j] + content[z] + content[i]
+            if tmp > 2020:
+                z -= 1
+            elif tmp < 2020:
+                j += 1
+            else:
+                print(content[j] * content[z] * content[i])
+                return
+
+task2()
