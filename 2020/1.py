28e3e6275788a9ed6d567d2e09a26dafa0205fd4
Mikael Mölder <mikael@molder.se>
2020-12-03 12:32:17 +0100

    CHG: day 1 in same format as rest

--- 2020/1.py
+++ 2020/1.py
@@ -5,6 +5,19 @@ with open("2020/input_1_1.csv", 'r') as file:
 
 content = sorted(content, key=lambda x: x)
 
+def task1():
+    i = 0
+    j = -1
+    while True:
+        if content[i] + content[j] == 2020:
+            print("%s" % (content[i] * content[j]))
+            break
+        elif content[i] + content[j] > 2020:
+            j-=1
+        elif content[i] + content[j] < 2020:
+            i+=1
+            j=-1
+
 def task2():
     for i in range(len(content)):
         j = i + 1
@@ -19,4 +32,5 @@ def task2():
                 print(content[j] * content[z] * content[i])
                 return
 
+task1()
 task2()
