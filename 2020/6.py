dfee4e00a1945984477c829408e19a5039f3f9b3
Mikael Mölder <mikael@molder.se>
2020-12-06 11:57:54 +0100

    ADD: day 6 solution

---
+++ 2020/6.py
@@ -0,0 +1,25 @@
+import string
+
+with open("2020/input_6.csv", 'r') as file:
+    lines = file.read()
+
+groups = lines.split('\n\n')
+ 
+def task1():
+    g = [group.replace('\n', '') for group in groups]
+    yes = sum(len(set(x)) for x in g)
+    print(yes)
+
+def task2():
+    g = [group.replace('\n', ' ').split(' ') for group in groups]
+    unique_yes = 0
+    for group in g:
+        res = set(group[0])
+        for x in group[1:]:
+            res &= set(x)
+        unique_yes += len(list(res))
+    print(unique_yes)
+    
+
+task1()
+task2()
